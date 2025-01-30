98f67af2b46bec9628dbe83c180fbfb4145e36e7
Jacob Cabrera <jacobcabrera718@gmail.com>
2025-01-30 15:58:31 -0500

    Add project files.

---
+++ eCommerce-Platform/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce_Platform.Models
+{
+    public class Product
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public Product()
+        {
+            Name = string.Empty;
+            Id = 0;
+        }
+
+        public string? Display
+        {
+            get
+            {
+                return $"{Id}. {Name}";
+            } 
+        }
+
+        public override string ToString()
+        {
+            return Display ?? string.Empty;
+        }
+
+
+
+
+
+    }
+}
---
+++ eCommerce-Platform/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Xml.Serialization;
+using eCommerce_Platform.Models;
+
+
+namespace eCommerce_Platform
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lastKey = 1;
+
+            //provide user menu
+            Console.WriteLine("Welcome to my Shop");
+            Console.WriteLine("1. Create new Inventory Item");
+            Console.WriteLine("2. Update an Inventory Item");
+            Console.WriteLine("3. Read all the Inventory Items");
+            Console.WriteLine("4. Delete an Inventory Item");
+            Console.WriteLine("0. Quit");
+
+            List<Product?> list = new List<Product?>();
+
+            char choice;
+
+            do
+            {
+
+                //take in user input 
+                string? input = Console.ReadLine();
+                choice = input[0];
+
+                //switch case
+                switch (choice)
+                {
+                    case '1':
+                        //create case
+                        //add to list
+                        list.Add(new Product
+                        {
+                            Id = lastKey++,
+                            Name = Console.ReadLine(),
+                        });
+                        break;
+                    case '2':
+                        //update case
+                        //select product
+                        Console.WriteLine("Which product would you like to update?");
+                        int selection = int.Parse(Console.ReadLine() ?? "-1");
+                        var selectedProduct = list.FirstOrDefault(p => p.Id == selection);
+                        if(selectedProduct != null)
+                        {
+                            selectedProduct.Name = Console.ReadLine() ?? "Error";
+                        }
+                        
+                        //replace with new product
+                        break;
+                    case '3':
+                        //read case
+                        //print out all the products
+                        list.ForEach(Console.WriteLine);
+                        break;
+                    case '4':
+                        //delete case
+                        //select and delete product from list
+                        Console.WriteLine("Which product would you like to delete?");
+                        selection = int.Parse(Console.ReadLine() ?? "-1");
+                        selectedProduct = list.FirstOrDefault(p => p.Id == selection);
+                        list.Remove(selectedProduct);
+                        break;
+                    case '0':
+                        break;
+                    default:
+                        Console.WriteLine("Error: Choose one of the commands shown");
+                        break;
+
+                }
+            } while (choice != '0');
+
+        }
+
+        static void AddProduct(List<string?> list)
+        {
+            var newProduct = Console.ReadLine();
+
+            list.Add(newProduct);
+        }
+    }
+}
